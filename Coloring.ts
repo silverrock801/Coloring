d8ba2c21eb7ef039ada4b9223fa60a0fbd507669
silverrock801 <jamesp801@gmail.com>
2022-02-22 10:19:54 -0500

    Finished code

---
+++ Coloring.ts
@@ -0,0 +1,30 @@
+
+
+
+function coloring(color: string[]) {
+
+    let time = 0;
+    let temp = " ";
+    for (let i = 0; i < color.length; i++ ){
+
+        if (i = 0){
+            time = time + 2;
+            temp = color[i];
+        }
+        else {
+            if (temp == color[i]) {
+                time = time + 2
+            }
+
+            else {
+                time = time + 3;
+            }
+            
+
+
+        }
+
+    }
+
+    return time;
+}
